ec4137206d4708554806d0947c7d9192c346e015
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-09-06 11:28:51 +0200

    task 1 done

--- Python/Labs/01_lab/1_task.py
+++ Python/Labs/01_lab/1_task.py
@@ -2,22 +2,34 @@ from pathlib import Path
 from glob import glob
 from sys import argv
 
+from shutil import copy  # to copy files
+
 def main():
+
+    size_check = 2048  # in bytes
     
     directory = Path('.')
     
     if len(argv) > 1:
         directory = Path(argv[1])
 
-    try:
-        for elem in directory.iterdir():
-            print(elem)
+        if not directory.exists():
+            print("Error, directory", directory, "was not found")
+            return
 
-    except Exception:
-        print("Error while accessing directory")
+    file_list = [file for file in directory.glob("*") if file.is_file() and file.stat().st_size < size_check]
 
-    finally:
+    if not file_list:
+        print("No files with size <", size_check, "bytes were found")
         return
+
+    Path("./small").mkdir(exist_ok=True)
+
+    for file in file_list:
+        print(file)
+
+        copy(file, Path("./small").joinpath(file.name))
+
 
 
 if __name__ == "__main__":
