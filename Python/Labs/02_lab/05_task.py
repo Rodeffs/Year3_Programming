7a5647a0f6ccf1c5b9b80d369eb129d3145d8b33
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-09-13 17:29:37 +0200

    task 5 done

--- Python/Labs/02_lab/05_task.py
+++ Python/Labs/02_lab/05_task.py
@@ -4,10 +4,28 @@
 
 from PIL import Image
 from argparse import ArgumentParser
+from pathlib import Path
 
 
 def main():
-    pass
+
+    parser = ArgumentParser()
+    parser.add_argument("-ftype", required=True, help="the filetype to search for")
+    parser.add_argument("-dir", default=".", help="dir to scan")
+
+    arg = parser.parse_args()
+
+    # Проверяем все файлы в cwd, если они указанного типа, то уменьшаем их до 50x50 и показываем
+
+    for file in Path(arg.dir).iterdir():
+        if file.is_file():
+            try:
+                with Image.open(file) as img:
+                    if img.format == arg.ftype:
+                        img.thumbnail((50, 50))
+                        img.show()
+            except:
+                continue
 
 
 if __name__ == "__main__":
