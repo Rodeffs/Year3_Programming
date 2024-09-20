b1fae8e6e583bac14a970e3de880c22c66366ff0
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-09-20 22:44:16 +0200

    task 2 progress

--- Python/Labs/03_lab/02_task.py
+++ Python/Labs/03_lab/02_task.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from skimage import io
+from skimage import exposure, io
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -12,17 +12,32 @@ def main():
     
     path = parser.parse_args().i
 
+    values, new_values = [], []
+
     with Image.open(path) as img:
-        histogram = img.histogram()
 
-        fig, axs = plt.subplots(1, 1)
+        values = img.histogram()
 
-        axs.hist(histogram)
+    # Суть в том, что Image.histogram() возвращает массив из 768 элементов для RGB картинок, где:
+    #   первые 256 элементов отвечают красному цвету
+    #   следующие 256 - зелёному
+    #   последние 256 - синему
+    # Сами индексы отвечают за яркость, а значение, которые они возвращают - кол-во пискелей такой яркости для каждого цвета
+    # Т.е. values[0] = кол-во пикселей с R = 0, values[256] = пиксели с G = 0 и values[512] = пиксели с B = 0
+    # Поэтому для получения гистограммы их надо сложить
 
-        plt.show()
+    fig, axs = plt.subplots(1, 1, squeeze=False)
 
+    for i in range(0, 256):
+        new_values.append(values[i] + values[i+256] + values[i+512])
 
+    img = io.imread(path)
+    
+    histogram = exposure.histogram(img, channel_axis=2)
 
+    plt.stairs(new_values, fill=True)
+
+    plt.show()
 
 
 if __name__ == "__main__":
