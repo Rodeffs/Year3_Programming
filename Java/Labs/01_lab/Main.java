ebea39f46a1dc8ffaba3f0e2c07c86bd0ce7577f
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-01-20 20:40:10 +0200

    task 1 done

--- Java/Labs/01_lab/Main.java
+++ Java/Labs/01_lab/Main.java
@@ -2,10 +2,58 @@ import java.util.Scanner;
 
 public class Main {
 
-    Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
 	
+	System.out.println("Введите номер задания (1-5):");
+	System.out.println("1. Сиракузская последовательность\n2. Сумма ряда\n3. Ищем клад\n4. Логистический максимин\n5. Дважды чётное число");
+
+	Scanner in = new Scanner(System.in);
+
+	int lab = in.nextInt();
+
+	switch(lab) {
+	    case 1:
+		System.out.println("Введите натуральное число:");
+		int n = in.nextInt();
+		collatz_conjecture(n);
+		System.out.println("Потребовалось " + count + " шагов");
+		break;
+
+	    case 2:
+		break;
+
+	    case 3:
+		break;
+
+	    case 4:
+		break;
+
+	    case 5:
+		break;
+
+	    default:
+		System.out.println("Ошибка: такого задания не было");
+	}
+
+	in.close();
+    }
+
+    static int count = 0;
+
+    static void collatz_conjecture(int n) {
+	if (n == 1) {
+	    return;
+	}
+	else if (n % 2 == 0) {
+	    n /= 2;
+	}
+	else {
+	    n = 3*n + 1;
+	}
+
+	count++;
+	collatz_conjecture(n);
     }
 
 
