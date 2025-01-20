e9c56f74706b84cebecf83ae81220bd2c202dc11
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-01-20 22:21:59 +0200

    all tasks except 3 done

--- Java/Labs/01_lab/Main.java
+++ Java/Labs/01_lab/Main.java
@@ -1,12 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-	
-	System.out.println("Введите номер задания (1-5):");
-	System.out.println("1. Сиракузская последовательность\n2. Сумма ряда\n3. Ищем клад\n4. Логистический максимин\n5. Дважды чётное число");
+	System.out.println("Задания:\n1. Сиракузская последовательность\n2. Сумма ряда\n3. Ищем клад\n4. Логистический максимин\n5. Дважды чётное число");
+	System.out.print("Введите номер задания (1-5): ");
 
 	Scanner in = new Scanner(System.in);
 
@@ -14,22 +13,65 @@ public class Main {
 
 	switch(lab) {
 	    case 1:
-		System.out.println("Введите натуральное число:");
-		int n = in.nextInt();
-		collatz_conjecture(n);
+		System.out.print("Введите натуральное число: ");
+
+		collatz_conjecture(in.nextInt());
+
 		System.out.println("Потребовалось " + count + " шагов");
 		break;
 
 	    case 2:
+		System.out.print("Введите количество чисел в знакочередующемся ряде: ");
+		int size = in.nextInt();
+		int[] numbers = new int[size];
+    
+		System.out.println("Введите " + size + " чисел:");
+
+		for (int i = 0; i < size; i++) {
+		    numbers[i] = in.nextInt();
+		}
+
+		System.out.println("Сумма ряда: " + row_sum(numbers));
 		break;
 
 	    case 3:
 		break;
 
 	    case 4:
+		System.out.print("Введите количество дорог: ");
+		int road_count = in.nextInt();
+		
+		ArrayList<ArrayList<Integer>> roads = new ArrayList<ArrayList<Integer>>();
+
+		for (int i = 1; i <= road_count; i++) {
+		    ArrayList<Integer> road = new ArrayList<Integer>();
+
+		    System.out.print("Введите количество туннелей для " + i + " дороги: ");
+		    int tunnel_count = in.nextInt();
+		    System.out.println("Введите высоту каждого туннеля:");
+
+		    for (int j = 0; j < tunnel_count; j++) {
+			road.add(in.nextInt());
+		    }
+
+		    roads.add(road);
+		}
+
+		int[] optimal = max_height(roads);
+		
+		String output = String.format("Оптимальные дорога и высота грузовика: %d %d", optimal[0], optimal[1]);
+		System.out.println(output);
 		break;
 
 	    case 5:
+		System.out.print("Введите положительное трёхзначное число: ");
+
+		if (twice_even(in.nextInt())) {
+		    System.out.println("Это дважды чётное число");
+		}
+		else {
+		    System.out.println("Это не дважды чётное число");
+		}
 		break;
 
 	    default:
@@ -56,5 +98,68 @@ public class Main {
 	collatz_conjecture(n);
     }
 
+    static boolean minus = false;
 
+    static int row_sum(int[] numbers) {
+	int sum = 0;
+
+	for (int number : numbers) {
+	    if (minus) {
+		sum -= number;
+		minus = false;
+	    }
+	    else {
+		sum += number;
+		minus = true;
+	    }
+	}
+
+	return sum;
+    }
+
+    static int find_min(ArrayList<Integer> array) {
+	int min = Integer.MAX_VALUE;
+
+	for (int i : array) {
+	    if (i < min) {
+		min = i;
+	    }
+	}
+
+	return min;
+    }
+
+    static int[] max_height(ArrayList<ArrayList<Integer>> roads) {
+	int[] optimal_road = new int[2];
+	int max = Integer.MIN_VALUE;
+
+	for (int i = 0; i < roads.size(); i++) {
+	    int lowest_tunnel = find_min(roads.get(i));
+
+	    if (lowest_tunnel > max) {
+		max = lowest_tunnel;
+		optimal_road[0] = i+1;
+		optimal_road[1] = max;
+	    }
+	}
+
+	return optimal_road;
+    }
+
+    static boolean twice_even(int number) {
+	int sum = 0, mult = 1;
+
+	while (number >= 1) {
+	    int digit = number % 10;
+	    sum += digit;
+	    mult *= digit;
+	    number /= 10;
+	}
+
+	if ((sum % 2 == 0) && (mult % 2 == 0)) {
+	    return true;
+	}
+
+	return false;
+    }
 }
