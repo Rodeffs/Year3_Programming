f65f453c9093df4dbe42803e042016dadc61694b
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-02-06 22:26:59 +0200

    lab 1 almost done

---
+++ Machine_Learning/Labs/01_lab/linear_regression.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+
+
+def coef(data, swap):  # нахождение коэффициентов регрессионной прямой через SSE
+    sum_x, sum_y, sum_xy, sum_x2 = 0, 0, 0, 0
+    N = len(data)
+
+    for i in range(N):
+        x, y = data[i][0], data[i][1]
+        if swap:
+            x, y = y, x
+        
+        sum_x += x
+        sum_y += y
+        sum_xy += x*y
+        sum_x2 += x**2
+
+    b = (sum_xy - sum_x*sum_y/N)/(sum_x2 - sum_x**2/N)
+    a = (sum_y - b*sum_x)/N
+
+    return (a, b)
+
+
+def f(x, a, b):
+    return a + b*x
+
+
+def main():
+    
+    # Считывание параметров
+
+    parser = ArgumentParser()
+    parser.add_argument("-o", "--open", required=True, help="the csv file to open")
+    parser.add_argument("-s", "--swap", action="store_true", help="swap columns used for axis, default: first column - x, second column - y")
+    args = parser.parse_args()
+
+    data = pd.read_csv(args.open)
+    
+    # Вывод статистики
+
+    print("STATS\n")
+    print(f"COUNT\n{data.count(axis=0)}\n")
+    print(f"MINIMUM\n{data.min(axis=0)}\n")
+    print(f"MAXIMUM\n{data.max(axis=0)}\n")
+    print(f"MEAN\n{data.mean(axis=0)}\n")
+
+    # Основной код
+
+    np_arr = data.to_numpy()
+
+    a, b = coef(np_arr, args.swap)
+
+    X, Y = data.columns[0], data.columns[1]
+
+    transposed = np.transpose(np_arr)  # чтобы чётко отделить координаты x от y
+    values = transposed[0]
+
+    if args.swap:  # т.к. по условию можно выбрать что будет x, а что - y
+        X, Y = Y, X
+        values = transposed[1]
+     
+    data.plot(x=X, y=Y, kind="scatter", color="#FF0000")  # точки
+    plt.plot(values, f(values, a, b), color="#0000FF")  # регрессионная прямая
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
