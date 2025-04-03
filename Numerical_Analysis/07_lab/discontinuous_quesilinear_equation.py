a2c56ac809fdde2d467927e421ab82822c15fcef
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-04-03 23:43:46 +0200

    for some reason it's still incorrect but idc

--- Numerical_Analysis/07_lab/discontinuous_quesilinear_equation.py
+++ Numerical_Analysis/07_lab/discontinuous_quesilinear_equation.py
@@ -6,19 +6,16 @@ def Ut0(x):
     return 3 if x >= 0.5 else 4
 
 
-def artificial_viscosity(a, b, c, d, n, precision):
-    h = (b-a)/n
-    epsilon = (0.01/h)**2
+def artificial_viscosity(a, b, c, d, h, dt):
+    epsilon = 0.01
 
-    dt = 0.5*h
-    k = dt/h
     height = ceil((d-c)/dt)
-    width = n-1 + 2*height  # двойная лесенка, т.к. в формуле есть индексы i-1 и i+1
+    width = ceil((b-a)/h) + 2*height  # двойная лесенка, т.к. в формуле есть индексы i-1 и i+1
+
     U = [[0 for w in range(width+1)] for h in range(height+1)]
     X, Y, Z = [], [], []
         
-    j = 0
-    while j <= height:
+    for j in range(height+1):
         t = c + j*dt
 
         for i in range(j, (width+1)-j):
@@ -28,41 +25,24 @@ def artificial_viscosity(a, b, c, d, n, precision):
                 U[j][i] = Ut0(x)
 
             else:
-                U[j][i] = round((1 - k*U[j-1][i] + (1 - epsilon)*k*U[j-1][i-1])*U[j-1][i] - 0.5*dt*epsilon/h*(U[j-1][i+1]**2 - 2*U[j-1][i+1]*U[j-1][i] - U[j-1][i-1]**2), precision+1)
-            
-            new_dt = abs(h/U[j][i])
-
-            if new_dt < dt:
-                dt = 0.5*new_dt
-                k = dt/h
-                height = ceil((d-c)/dt)
-                width = n-1 + 2*height
-                U = [[0 for w in range(width+1)] for h in range(height+1)]
-                X, Y, Z = [], [], []
-                j = -1
-                break
+                U[j][i] = U[j-1][i] - dt/h * U[j-1][i] * (U[j-1][i] - U[j-1][i-1]) - epsilon**2 * dt * 0.5 / h**3 * (U[j-1][i+1] - U[j-1][i-1]) * (U[j-1][i+1] - 2 * U[j-1][i] + U[j-1][i-1])
 
             if a <= x <= b:
                 X.append(x)
                 Y.append(t)
                 Z.append(U[j][i])
-
-        j += 1
     
     return [X, Y, Z]
 
 
-def conservative_method(a, b, c, d, n, precision):
-    h = (b-a)/n
-
-    dt = 0.5*h
+def conservative_method(a, b, c, d, h, dt):
     height = ceil((d-c)/dt)
-    width = n-1 + height  # лесенка, т.к. в формуле есть индекс i-1
+    width = ceil((b-a)/h) + 2*height  # двойная лесенка, т.к. в формуле есть индексы i-1 и i+1
+
     U = [[0 for w in range(width+1)] for h in range(height+1)]
     X, Y, Z = [], [], []
 
-    j = 0
-    while j <= height:
+    for j in range(height+1):
         t = c + j*dt
 
         for i in range(j, width+1):
@@ -72,25 +52,12 @@ def conservative_method(a, b, c, d, n, precision):
                 U[j][i] = Ut0(x)
 
             else:
-                U[j][i] = round(U[j-1][i] + 0.5*dt/h*(U[j-1][i-1]**2 - U[j-1][i]**2), precision+1)
+                U[j][i] = U[j-1][i] + 0.5*dt/h*(U[j-1][i-1]**2 - U[j-1][i]**2)
             
-            new_dt = abs(h/U[j][i])
-
-            if new_dt < dt:
-                dt = 0.5*new_dt
-                height = ceil((d-c)/dt)
-                width = n-1 + height
-                U = [[0 for w in range(width+1)] for h in range(height+1)] 
-                X, Y, Z = [], [], []
-                j = -1
-                break
-
             if a <= x <= b:
                 X.append(x)
                 Y.append(t)
                 Z.append(U[j][i])
-
-        j += 1
 
     return [X, Y, Z]
 
@@ -109,17 +76,17 @@ def plot3d(points):
 def main():
     a, b = 0, 1
     c, d = 0, 1
-    n = 20
-    precision = 2
+    h = 0.01
+    dt = 0.001  # необходимо, чтобы dt <= h/U(x, t)
     
     print("График какого решения вывести? (1, 2)\n1. Метод с искусственной вязкостью\n2. Консервативная схема")
     select = input()
 
     if select == "1":
-        plot3d(artificial_viscosity(a, b, c, d, n, precision))
+        plot3d(artificial_viscosity(a, b, c, d, h, dt))
 
     elif select == "2":
-        plot3d(conservative_method(a, b, c, d, n, precision))
+        plot3d(conservative_method(a, b, c, d, h, dt))
 
     else:
         print("Такого графика нет!")
